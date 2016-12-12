02e5ee3cc06f8ee340f24514ca87e949d55879f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-12-12 13:50:45 +0100

    Parsing GitLab data

--- src/branch_handler/core.clj
+++ src/branch_handler/core.clj
@@ -36,6 +36,54 @@
     (-> (http-response/response response-text)
         (http-response/content-type "text/html")))
 
+(defn parse-gitlab-info
+    [request]
+    (let [body (read-request-body request)]
+        (if body
+            (json/read-str body :key-fn clojure.core/keyword))))
+
+(def empty-SHA "0000000000000000000000000000000000000000")
+
+(defn get-operation
+    [before after]
+    (if (= before empty-SHA)
+        (if (= after empty-SHA)
+            :unknown
+            :branch-created)
+        (if (= after empty-SHA)
+            :branch-deleted
+            :branch-updated)))
+
+(defn get-branch
+    [git-ref]
+    (try
+        (subs git-ref (inc (.lastIndexOf git-ref "/")))
+        (catch Exception e
+            nil)))
+
+(defn api-call-handler
+    [request]
+    (let [gitlab-info (parse-gitlab-info request)
+          repository-url   (-> gitlab-info :repository :url)
+          repository-name  (-> gitlab-info :repository :name)
+          repository-group (-> gitlab-info :project :namespace)
+          branch           (get-branch (-> gitlab-info :ref))
+          before           (-> gitlab-info :before)
+          after            (-> gitlab-info :after)
+          operation        (get-operation before after)]
+        (if verbose
+            (clojure.pprint/pprint gitlab-info))
+        (log/info "repository" repository-name)
+        (log/info "URL"        repository-url)
+        (log/info "group"      repository-group)
+        (log/info "branch"     branch)
+        (log/info "before"     before)
+        (log/info "after"      after)
+        (log/info "operation"  operation)
+        ))
+
+        
+
 (defn http-request-handler
     "Handler that is called by Ring for all requests received from user(s)."
     [request]
