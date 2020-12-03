c2c62f62f951b947c3653c4561ac561099ba0007
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-03 08:25:28 +0100

    Project description

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
 ;
 
 (defproject branch-handler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Git branch handler service"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
